e99c34d9389e0fcc5f081792f397458cd8c79879
Elton Mozart <elton.m.da.silva@accenture.com>
2022-03-20 22:27:34 -0300

    Resolução do desafio: implementação das funções handleCreateNewTask, handleToggleTaskCompletion e handleRemoveTask.

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,27 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle) {
+      const newTask: Task = {
+        id: Math.floor(Math.random() * 100000),
+        title: newTaskTitle,
+        isComplete: false
+      }
+  
+      setTasks([...tasks, newTask]);
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const index = tasks.findIndex(task => task.id === id);
+    const updatedTasks = [...tasks];
+    updatedTasks[index].isComplete = !updatedTasks[index].isComplete;
+    setTasks(updatedTasks);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const updatedTasks = tasks.filter(task => task.id !== id);
+    setTasks(updatedTasks);
   }
 
   return (
